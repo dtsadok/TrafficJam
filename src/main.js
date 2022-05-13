868f4f59b9e6a80a6c46ad12433ae7fbacefb284
Daniel <daniel@MSI.home>
2022-05-13 00:59:37 -0400

    We have working game, yay!  Now we just need puzzles to solve... Next step is loading those dynamically from a list.

--- src/main.js
+++ src/main.js
@@ -1,11 +1,117 @@
-//red car is assumed to be horizontal, size 2, and on row 3
+//TODO: move this to JSON
 const Pieces = {
 	red: { col: 3 },
-	pc1: { col: 1, row: 4, orient: "vert", size: 3 },
-	pc2: { col: 5, row: 6, orient: "horiz", size: 2 }
+	pc1: { col: 1, row: 1, orient: "vert", size: 3 },
+	pc2: { col: 6, row: 3, orient: "vert", size: 2 },
+	pc3: { col: 1, row: 1, orient: "horiz", size: 2 }
 };
+const Stuff = Pieces;  //because we cant tye allow asting
+
+//red car must be horizontal, size 2, and on row 3
+Pieces.red.row = 3;
+Pieces.red.orient = "horiz";
+Pieces.red.size = 2;
+
+//returns object indicating whether spaces are available or not
+function getAvailableSpaces(pieces) {
+	//init grid info
+	let grid = {};
+
+	for (let i=1; i<=6; i++) {
+		for (let j=1; j<=6; j++) {
+			grid[`${i},${j}`] = true;
+		}
+	}
+
+	//go through each piece and mark the
+	//spaces they take up as not available
+	for (let id in pieces) {
+		let pc = pieces[id];
+
+		for (let i=0; i<pc.size; i++) {
+			if (pc.orient === "horiz") {
+				grid[`${pc.col+i},${pc.row}`] = false;
+			} else if (pc.orient === "vert") {
+				grid[`${pc.col},${pc.row+i}`] = false;
+			}
+		}
+	}
+
+	return grid;
+}
+
+//show moves for a piece.
+//should look like ["1,2", "1,3", etc],
+//where each item represents a new coordinate for the piece.
+function getPossibleMoves(pieceId, pieces) {
+	let moves = [];
+	const pc = pieces[pieceId];
+
+	const availableSpaces = getAvailableSpaces(pieces);
+
+	if (pc.orient === "horiz") {
+		//check spaces to the left
+		for (let i=pc.col-1; i>=1; i--) {
+			const pos = `${i},${pc.row}`;
+			//is the space available (i.e. not blocked?)
+			if (availableSpaces[pos]) {
+				moves.push(pos);
+			} else { //once we're blocked we can't move any further
+				break;
+			}
+		}
+
+		//to the right
+		for (let i=pc.col+pc.size; i<=6; i++) {
+			if (availableSpaces[`${i},${pc.row}`]) {
+				//we are moving from the left, so take that into account
+				moves.push(`${i-pc.size+1},${pc.row}`);
+			} else {
+				break;
+			}
+		}
+	} else if (pc.orient === "vert") {
+		//check up
+		for (let i=pc.row-1; i>=1; i--) {
+			const pos = `${pc.col},${i}`;
+			if (availableSpaces[pos]) {
+				moves.push(pos);
+			} else {
+				break;
+			}
+		}
+
+		//check down
+		for (let i=pc.row+pc.size; i<=6; i++) {
+			if (availableSpaces[`${pc.col},${i}`]) {
+				//we are moving from the top, so take that into account
+				moves.push(`${pc.col},${i-pc.size+1}`);
+			} else {
+				break;
+			}
+		}
+	}
+
+	return moves;
+}
+
+function movePiece(pieceId, pieces, newPos) {
+	const pos = newPos.split(",").map(x => parseInt(x));
+	pieces[pieceId].col = pos[0];
+	pieces[pieceId].row = pos[1];
+}
+
+//use this if we want to exit the red car automatically
+function canLeave(pieces) {
+	const moves = getPossibleMoves("red", pieces);
+	return moves.indexOf("5,3") >= 0;
+}
+
+function isWinner(pieces) {
+	return (pieces.red.col === 5 && pieces.red.row === 3);
+}
 
 window.onload = () => {
-	const board = document.querySelector("#board");
+	const board = document.getElementById("board");
 	renderBoard(board, Pieces);
 }
--- src/ui.js
+++ src/ui.js
@@ -18,12 +18,18 @@ function renderBoard(boardElement, pieces) {
 		if (p.orient) {
 			newPiece.classList.add(p.orient);
 		}
+		
+		newPiece.addEventListener("click", ev => {
+			selectPiece(ev.target.id, pieces);
+		});
 
 		boardElement.appendChild(newPiece);
 	}
 
 	//move pieces to where they need to go
 	updateBoard(pieces);
+	
+	initAvailableMoveElements(boardElement);
 }
 
 function updateBoard(pieces) {
@@ -37,4 +43,92 @@ function updateBoard(pieces) {
 			e.style.gridRowStart = p.row;
 		}
 	}
+}
+
+//create elements to visualize possible moves
+function initAvailableMoveElements(boardElement) {
+	for (let i=1; i<=6; i++) {
+		for (let j=1; j<=6; j++) {
+			const e = document.createElement("div");
+			e.id = `available-${i}-${j}`;
+			e.className = "available";
+			//only show these elements when a piece is selected,
+			//and the space represented by the element in question is
+			//one of the possible moves for that piece. 
+			e.style.display = "none";
+
+			boardElement.appendChild(e);
+		}
+	}
+}
+
+function deselectAll() {
+	//remove selected class from all elements
+	const selected = document.getElementsByClassName("selected");
+	for (e of selected) {
+		e.classList.remove("selected");
+	}
+
+	//hide available moves squares
+	const available = document.getElementsByClassName("available");
+	for (e of available) {
+		e.style.display = "none";
+	}
+}
+
+function selectPiece(pieceId, pieces) {
+	const el = document.getElementById(pieceId);
+	const isSelected = el.classList.contains("selected");
+
+	deselectAll();
+
+	//if already selected, then deselect and go
+	if (isSelected) { return; }
+
+	el.classList.add("selected");
+
+	const pc = pieces[pieceId];
+
+	const possibleMoves = getPossibleMoves(pieceId, pieces);
+	for (let move of possibleMoves) {
+		const availableElement = getAvailableElementForMove(move, pc);
+		availableElement.style.display = "block";
+
+		//event handler needs to be a singleton,
+		//so use onclick instead of addEventListener
+		availableElement.onclick = function() {
+			deselectAll();
+			movePiece(pieceId, pieces, move);
+			updateBoard(pieces);
+
+			//did we win with this move?
+			if (isWinner(pieces)) {
+				const redElement = document.getElementById("red");
+				const boardElement = redElement.parentElement;
+				redElement.classList.add("free");
+				boardElement.classList.add("winner");
+			}
+		}
+	}
+}
+
+//pieces move based on their top-left position, but that doesn't look
+//good in the UI: instead, we want availablity hints to show up on
+//either side of the piece
+function getAvailableElementForMove(move, piece) {
+	//adjust position of possible move (which is
+	//relative to top-left of piece) for UI (so squares look good)
+	const pos = move.split(",").map(x => parseInt(x));
+
+	if (pos[0] > piece.col) {
+		pos[0] += piece.size;
+		pos[0]--;
+	} else if (pos[1] > piece.row) {
+		pos[1] += piece.size;
+		pos[1]--;
+	}
+
+	//map to element ID
+	const availableId = "available-" + pos.join("-");
+	return document.getElementById(availableId);
 }
